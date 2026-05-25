fb8841e0d1a00b11f4d940bd15ee08a752a6ffe0
justJS <jserrano.dev@hotmail.com>
2026-05-25 16:15:56 -0400

    Added login endpoint

--- users/views.py
+++ users/views.py
@@ -5,11 +5,25 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .serializers import RegisterSerializer, UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+class LoginSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Aquí puedes agregar datos extra al token si lo deseas
+        token['username'] = user.username
+        token['is_teacher'] = user.is_teacher
+        return token
+
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
 
 class UserDetailView(APIView):
     permission_classes = (IsAuthenticated,)
