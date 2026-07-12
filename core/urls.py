5ff729189adbab8cabd8055046f15a40819fa1ee
justJS <jserrano.dev@hotmail.com>
2026-07-12 02:23:47 -0400

    moved google auth from courses to core app

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import RegisterView, UserDetailView
+from courses.views import google_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/me/', UserDetailView.as_view(), name='auth_me'),
+    path('api/auth/google/', google_login, name='google-login'),
     path('api/users/', include('users.urls')),
 ]
